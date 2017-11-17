15971e653cef10c2b8bb065f08c6783da2264a35
Dmitry Lunin <dimon2016@mail.ru>
2017-11-17 20:07:44 +0300

    Mersedes-bens 540

---
+++ task3/app.js
@@ -0,0 +1,18 @@
+function addList(list, text) {
+    var par = list.append('<li></li>').find('li').last();
+    par.append('<span>' + text + '</span>');
+    par.append('<input type="button" id="clear" value="Удалить">');
+}
+$(document).ready(function () {
+    var list = $("#root").append('<ul></ul>').find('ul');
+    addList(list, "Сделать задание #3 по web-программированию");
+    $('#root').append('<input type="input" id="add_task_input">');
+    $('#root').append('<input type="button" id="add_task" value="OK">');
+    $("#add_task").click(function () {
+        addList(list, $('#add_task_input').val());
+    });
+    $("#root").on('click', '#clear', function () {
+        $(this).parent().get(0).remove();
+    });
+});
+
