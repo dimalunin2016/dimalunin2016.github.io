9851f5bbb847f6588e4a829c7e46da2eaa1ba4dc
Dmitry Lunin <dimon2016@mail.ru>
2017-11-18 16:53:11 +0300

    Commit

--- task3/app.js
+++ task3/app.js
@@ -1,13 +1,16 @@
 function addList(list, text) {
-    var par = list.append('<li></li>').find('li').last();
+    var par = $('<li>');
     par.append('<span>' + text + '</span>');
-    par.append('<button class="deleter">').text("Удалить");
-    $(".deleter").click(function () {
+    par.append(
+            $('<button class="deleter">')
+        .text("Удалить")
+        .click(function () {
         $(this).parent().remove();
-    });
+    }));
+    $("#task_list").append(par);
 }
 $(document).ready(function () {
-    var list = $("#root").append('<ul></ul>').find('ul');
+    var list = $("#root").append('<ul id="task_list"></ul>');
     addList(list, "Сделать задание #3 по web-программированию");
     $('#root').append('<input type="input" id="add_task_input">');
     $('#root').append('<input type="button" id="add_task" value="OK">');
